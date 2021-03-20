db87a66bcc4d8ebb0ced16365fc3121d2ce9bd3d
lokpavan03 <80965262+lokpavan03@users.noreply.github.com>
2021-03-20 13:48:15 +0530

    Create main.tf

---
+++ terrraform/resourcegroup/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "remote" {
+    organization = "loktf"
+
+    workspaces {
+      name = "terraform"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+#Create a Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "WEgroup"
+  location = "West Europe"
+}
