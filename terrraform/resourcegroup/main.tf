507e95aaad114f7f79154be88a0853607df2d9cd
lokpavan03 <80965262+lokpavan03@users.noreply.github.com>
2021-03-20 14:04:30 +0530

    Update main.tf

--- terrraform/resourcegroup/main.tf
+++ terrraform/resourcegroup/main.tf
@@ -19,6 +19,6 @@ provider "azurerm" {
 
 #Create a Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = "TerraformCloudRG"
+  name     = "TerraformCloudRG01"
   location = "West Europe"
 }
